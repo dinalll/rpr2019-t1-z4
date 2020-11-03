faf937a785a333c58687dec4abc6da6b2f118060
Aldin Kozica <akozica1@etf.unsa.ba>
2020-11-03 20:28:20 +0100

    this is final commit without errors

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,15 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    int max_artikl=0;
     Artikl[] niz_artikl;
-    public void dodajArtikl(Artikl novi_artikl){
-        for(int i=0;<1000;i++){
-            if(niz_artikl[i]==null)niz_artikl=niz_artikl;
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl povratni=null;
+        for(int i=0;i<1000;i++){
+            if(niz_artikl[i].Kod.equals(kod)){
+                povratni=niz_artikl[i];
+                niz_artikl[i]=null;
+            }
         }
+        return povratni;
     }
-    public Artikl izbaciArtiklSaKodom(int kod){
-        for(int i=0;<1000;i++){
-            if(niz_artikl[i].Kod==kod)niz_artikl=null;
-        }
+    public boolean dodajArtikl(Artikl a){
+        if(max_artikl==1000)return false;
+        else for (int i = 0; i < 1000; i++)
+            if (niz_artikl[i] == null) {
+                niz_artikl[i] = a;
+                max_artikl++;
+                return true;
+            }
+        return true;
     }
+    public Artikl[] getArtikli(){
+
+        return niz_artikl;
+    }
+
 }
